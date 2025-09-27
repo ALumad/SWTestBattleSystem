17cf8661ab5c20f8af04ab6f77e0b8695035fffd
Aleksandr Gusev <108462407+ALumad@users.noreply.github.com>
2025-09-27 20:51:57 +0400

    Add files via upload

--- src/Core/System/System.cpp
+++ src/Core/System/System.cpp
@@ -8,131 +8,14 @@
 
 #include <algorithm>
 #include <cstdint>
+#include <ranges>
 
 namespace sw::core
 {
 
-	uint32_t MarchSystem::Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const
+	bool FinalizeMoveSystem::SkipEntity(const std::shared_ptr<Entity>& entity) const
 	{
-		uint32_t acted = 0;
-		for (auto& entity : entities)
-		{
-			if (entity->ContainsComponent(ComponentType::HasActed))
-			{
-				continue;
-			}
-			if (!entity->ContainsComponent(ComponentType::March))
-			{
-				continue;
-			}
-			if (!entity->ContainsComponent(ComponentType::Position))
-			{
-				throw std::logic_error("Entity can move, but doesn't have position");
-			}
-
-			auto& march = entity->GetComponent<March>(ComponentType::March);
-			auto& pos = entity->GetComponent<Position>(ComponentType::Position);
-
-			pos.x += utils::GetMoveStep(pos.x, march.target.x, march.speed);
-			pos.y += utils::GetMoveStep(pos.y, march.target.y, march.speed);
-
-			EventLog::log(tick, io::UnitMoved{entity->Id, static_cast<uint32_t>(pos.x), static_cast<uint32_t>(pos.y)});
-
-			if (pos == march.target)
-			{
-				entity->RemoveComponent(ComponentType::March);
-				EventLog::log(
-					tick, io::MarchEnded{entity->Id, static_cast<uint32_t>(pos.x), static_cast<uint32_t>(pos.y)});
-			}
-
-			entity->AddComponent(ComponentType::HasActed, HasActed{});
-			++acted;
-		}
-		return acted;
-	}
-
-	std::optional<io::UnitAttacked> AttackSystem::DoAttack(
-		std::shared_ptr<Entity>& entity, std::vector<std::shared_ptr<Entity>>& entities)
-	{
-		if (entity->ContainsComponent(ComponentType::Melee))
-		{
-			auto enemies = utils::FindNearbyEntities(entity, entities, 0, 1);
-			std::shared_ptr<Entity> enemy = utils::GetRandomEntity(enemies);
-			if (enemy)
-			{
-				auto& melee = entity->GetComponent<Melee>(ComponentType::Melee);
-				auto& enemy_health = enemy->GetComponent<Health>(ComponentType::Health);
-				auto damage = std::min(melee.strength, enemy_health.health);
-				enemy_health.health -= damage;
-				if (enemy_health.health == 0)
-				{
-					enemy->AddComponent(ComponentType::Dead, Dead{});
-				}
-				return io::UnitAttacked{entity->Id, enemy->Id, damage, enemy_health.health};
-			}
-		}
-
-		if (!entity->ContainsComponent(ComponentType::Range))
-		{
-			return {};
-		}
-
-		auto& range = entity->GetComponent<Range>(ComponentType::Range);
-
-		auto enemies = utils::FindNearbyEntities(entity, entities, range.range_from, range.range_to);
-		std::shared_ptr<Entity> enemy = utils::GetRandomEntity(enemies);
-		if (!enemy)
-		{
-			return {};
-		}
-
-		auto& enemy_health = enemy->GetComponent<Health>(ComponentType::Health);
-		auto damage = std::min(range.agility, enemy_health.health);
-		enemy_health.health -= damage;
-		if (enemy_health.health == 0)
-		{
-			enemy->AddComponent(ComponentType::Dead, Dead{});
-		}
-		return io::UnitAttacked{entity->Id, enemy->Id, damage, enemy_health.health};
-	}
-
-	uint32_t AttackSystem::Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const
-	{
-		uint32_t acted = 0;
-		// Find enemies for attask is O(n) => this mehthod has O(n^2) complexity
-		// It possible improve if use grid/map with coords
-		for (auto& entity : entities)
-		{
-			if (entity->ContainsComponent(ComponentType::HasActed))
-			{
-				continue;
-			}
-
-			bool can_attack
-				= entity->ContainsComponent(ComponentType::Melee) | entity->ContainsComponent(ComponentType::Range);
-
-			if (!can_attack)
-			{
-				continue;
-			}
-
-			if (!entity->ContainsComponent(ComponentType::Position))
-			{
-				throw std::logic_error(
-					"std::shared_ptr<Entity> doesn't have Position component and can't detect enemy arround him");
-			}
-			auto event = DoAttack(entity, entities);
-
-			if (!event)
-			{
-				continue;
-			}
-
-			EventLog::log(tick, std::move(event.value()));
-			entity->AddComponent(ComponentType::HasActed, HasActed{});
-			++acted;
-		}
-		return acted;
+		return !entity->ContainsComponent(ComponentType::HasActed);
 	}
 
 	uint32_t FinalizeMoveSystem::Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const
@@ -153,14 +36,23 @@ namespace sw::core
 
 		entities.erase(end_it, entities.end());
 
-		for (auto& entity : entities)
-		{
-			if (entity->ContainsComponent(ComponentType::HasActed))
+		std::for_each(
+			entities.begin(),
+			entities.end(),
+			[&](std::shared_ptr<Entity>& e)
 			{
-				entity->RemoveComponent(ComponentType::HasActed);
-			}
-		}
+				if (SkipEntity(e))
+				{
+					return;
+				}
+				e->RemoveComponent(ComponentType::HasActed);
+			});
 		return entities.size();
+	}
+
+	bool ScoreSystem::SkipEntity([[maybe_unused]] const std::shared_ptr<Entity>& entity) const
+	{
+		return false;
 	}
 
 	uint32_t ScoreSystem::Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const
@@ -169,17 +61,10 @@ namespace sw::core
 		{
 			return 0;
 		}
-		uint32_t total_score = 0;
-
-		// O(n^2) for the same reason as above
-		for (auto& entity : entities)
-		{
-			if (utils::HasNextMoveActivity(entity, entities))
-			{
-				++total_score;
-			}
-		}
-		return total_score;
+		return std::count_if(
+			entities.begin(),
+			entities.end(),
+			[&](std::shared_ptr<Entity>& e) { return utils::HasNextMoveActivity(e, entities); });
 	}
 
 }  // sw::core
--- src/Core/System/System.hpp
+++ src/Core/System/System.hpp
@@ -3,38 +3,35 @@
 
 #include <cstdint>
 #include <memory>
-#include <optional>
 #include <vector>
 
 namespace sw::core
 {
 	struct Entity;
 
+	enum class SystemCategory : int
+	{
+		Effect,
+		Movement,
+		Internal,
+	};
+
 	struct ISystem
 	{
 		virtual uint32_t Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const = 0;
+		virtual bool SkipEntity(const std::shared_ptr<Entity>& entity) const = 0;
 		virtual ~ISystem() = default;
-	};
-
-	struct MarchSystem : ISystem
-	{
-		uint32_t Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const override;
-	};
-
-	struct AttackSystem : ISystem
-	{
-		uint32_t Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const override;
-		static std::optional<io::UnitAttacked> DoAttack(
-			std::shared_ptr<Entity>& entity, std::vector<std::shared_ptr<Entity>>& entities);
 	};
 
 	struct FinalizeMoveSystem : ISystem
 	{
 		uint32_t Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const override;
+		bool SkipEntity(const std::shared_ptr<Entity>& entity) const override;
 	};
 
 	struct ScoreSystem : ISystem
 	{
 		uint32_t Apply(uint32_t tick, std::vector<std::shared_ptr<Entity>>& entities) const override;
+		bool SkipEntity(const std::shared_ptr<Entity>& entity) const override;
 	};
 }  // sw::core
